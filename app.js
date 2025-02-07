b33c140b5362ce5fb8c05ebe010ad9458092c36a
Muhammad Rasikh Riaz. <mo.rasikh11@gmail.com>
2025-02-07 10:23:18 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js with CI/CD Pipeline! By Muhammad Rasikh Riaz Triggered by successfully github webhook Automation.');
+  res.send('Hello, Node.js with CI/CD Pipeline! By Muhammad Rasikh Riaz Triggered by successfully github webhook Automation.i..');
 });
 
 const port = 3000;
