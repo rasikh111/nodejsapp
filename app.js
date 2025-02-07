aa0e3f997dd4632d07dd1b90c6cc38c82caab6f7
Muhammad Rasikh Riaz. <mo.rasikh11@gmail.com>
2025-02-07 10:29:21 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+// Import the express module
 const express = require('express');
 const app = express();
 
+// Define a route for the root path
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js with CI/CD Pipeline! By Muhammad Rasikh Riaz Triggered by successfully github webhook Automation.i..');
+  res.send('Hello, Node.js with CI/CD Pipeline! By Muhammad Rasikh Riaz Triggered by successfully github webhook Automation.');
 });
 
+// Set the port for the application
 const port = 3000;
+
+// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
