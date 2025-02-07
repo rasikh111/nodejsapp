cef93e0e0dbcf770f18ad107275c667647d9fe33
Muhammad Rasikh Riaz. <mo.rasikh11@gmail.com>
2025-02-07 10:40:15 +0000

    Update app.js, and Add public, and views.

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// Import the express module
 const express = require('express');
+const path = require('path');
 const app = express();
 
-// Define a route for the root path
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello, Node.js with CI/CD Pipeline! By Muhammad Rasikh Riaz Triggered by successfully github webhook Automation.');
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// Set the port for the application
 const port = 3000;
-
-// Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });
-
